a5da593d509be1b04e0d46dcaef926dae521063f
Sumit Hingu <sp.hingu@gmail.com>
2015-08-26 15:16:54 -0700

    :eight_spoked_asterisk:  Added proper transition effect with uihook

---
+++ client/views/common/layout.js
@@ -0,0 +1,15 @@
+Template.layout.onRendered(function () {
+    this.find('.main')._uihooks = {
+        insertElement: function (node, next) {
+            $(node).hide().insertBefore(next)
+            .delay(200)
+                .velocity('fadeIn', 1000);
+        },
+        removeElement: function (node) {
+            $(node).css('position','absolute').velocity({ translateX: '-1000px' }, {
+                duration: 400,
+                complete: function () { $(this).remove(); }
+            });
+        }
+    }
+});
