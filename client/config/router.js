33962e855a9ba040378c43c5de687abdd24fab73
Sumit Hingu <sp.hingu@gmail.com>
2015-08-25 23:57:26 -0700

    :tokyo_tower: Added page Transition effect

--- client/config/router.js
+++ client/config/router.js
@@ -1,5 +1,11 @@
+/* global Transitioner */
 /* global Router */
 Router.configure({
     layoutTemplate: 'layout',
     notFoundTemplate: 'notFound'
-})
+});
+
+Transitioner['default']({
+	  'in': 'transition.slideRightBigIn',
+    'out': 'transition.slideLeftBigOut'
+});
